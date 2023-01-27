453a435200675fa952c314b4b7e223d2fc3924e5
Mirko <mirko.menegazzo@gmail.com>
2023-01-27 17:49:57 +0100

    Added material icons to bulk uploads options

--- resources/views/project/manage_entries/bulk_upload_tab_panel.blade.php
+++ resources/views/project/manage_entries/bulk_upload_tab_panel.blade.php
@@ -3,25 +3,29 @@
         <div class="panel-body">
             <p>{{ trans('site.bulk_upload_description') }}</p>
             <p>
-            <div class="btn-group bulk-upload-btns"
-                 role="group"
-                 data-project-slug="{{$projectSlug}}"
-            >
-                @foreach($can_bulk_upload_enums as $value)
-                    <div data-bulk-upload="{{$value}}"
-                         class="btn btn-default btn-sm
-                         @if($value === $can_bulk_upload) btn-action @else '' @endif"
-                    >
-                        {{trans('site.'.$value)}}
+            <div class="btn-group bulk-upload-btns" role="group" data-project-slug="{{ $projectSlug }}">
+                @foreach ($can_bulk_upload_enums as $value)
+                    <div data-bulk-upload="{{ $value }}"
+                        class="btn btn-default btn-sm
+                         @if ($value === $can_bulk_upload) btn-action @else '' @endif">
+
+                        @if ($value === 'nobody')
+                            <i class="material-icons">person_off</i>
+                        @endif
+                        @if ($value === 'members')
+                            <i class="material-icons">people</i>
+                        @endif
+                        @if ($value === 'everybody')
+                            <i class="material-icons">public</i>
+                        @endif
+                        {{ trans('site.' . $value) }}
                     </div>
                 @endforeach
             </div>
             </p>
             <p class="well">
                 <strong>
-                    <a href="https://docs.epicollect.net/web-application/bulk-uploads"
-
-                    >More info on bulk uploads</a>
+                    <a href="https://docs.epicollect.net/web-application/bulk-uploads">More info on bulk uploads</a>
                 </strong>
             </p>
         </div>
