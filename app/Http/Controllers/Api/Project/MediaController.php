e8cf8b3a915ef0aca4b9a7b4e4df09f42a08c6dc
Mirko <mirko.menegazzo@gmail.com>
2023-03-22 16:27:25 +0100

    dataviewer 0.0.4

--- app/Http/Controllers/Api/Project/MediaController.php
+++ app/Http/Controllers/Api/Project/MediaController.php
@@ -40,6 +40,7 @@ class MediaController extends ProjectControllerBase
         $input = $request->all();
         $inputType = $input['type'];
 
+
         // Validate the options
         $mediaValidator->validate($input);
         if ($mediaValidator->hasErrors()) {
@@ -73,9 +74,9 @@ class MediaController extends ProjectControllerBase
             // Attempt to retrieve media
             try {
                 // Use the provided 'format' as the driver
-                $file = Storage::disk($input['format'])->get($this->requestedProject->ref . '/' . $input['name']);
+                $file = Storage::disk($format)->get($this->requestedProject->ref . '/' . $input['name']);
                 //get storage real path
-                $filepath = Storage::disk($input['format'])->getAdapter()->getPathPrefix();
+                $filepath = Storage::disk($format)->getAdapter()->getPathPrefix();
                 //get file real path
                 $filepath = $filepath . $this->requestedProject->ref . '/' . $input['name'];
                 //stream only audio and video
@@ -165,7 +166,7 @@ class MediaController extends ProjectControllerBase
             // Attempt to retrieve media
             try {
                 // Use the provided 'format' as the driver
-                $file = Storage::disk($input['format'])->get($this->requestedProject->ref . '/' . $input['name']);
+                $file = Storage::disk($format)->get($this->requestedProject->ref . '/' . $input['name']);
                 $response = Response::make($file, 200);
                 $response->header("Content-Type", $contentType);
 
