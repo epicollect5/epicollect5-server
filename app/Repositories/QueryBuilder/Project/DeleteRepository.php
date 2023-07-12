e31c73fdd2e14947674f6eae61953fc45a7c73f5
mirko77 <mirko.menegazzo@gmail.com>
2023-07-12 17:47:28 +0200

    Fixed bug with deletion bailing out if folder did not exist

--- app/Repositories/QueryBuilder/Project/DeleteRepository.php
+++ app/Repositories/QueryBuilder/Project/DeleteRepository.php
@@ -107,20 +107,22 @@ class DeleteRepository extends Base
      */
     private function deleteMedia($projectRef, $drivers)
     {
-        // Loop each driver
-        foreach ($drivers as $driver) {
-            // Get disk, path prefix and all directories for this driver
-            $disk = Storage::disk($driver);
-            $pathPrefix = $disk->getDriver()->getAdapter()->getPathPrefix();
-
-            // Note: need to use File facade here, as Storage doesn't delete
-            $deleted = File::deleteDirectory($pathPrefix . $projectRef);
-
-            //If the delete failed, add error and return (inform user to contact the administrator)
-            if (!$deleted) {
-                $this->errors = ['project_delete_media' => ['ec5_223']];
-                return;
+        // Loop each driver (folder) and delete it
+        try {
+            foreach ($drivers as $driver) {
+                // Get disk, path prefix and all directories for this driver
+                $disk = Storage::disk($driver);
+                $pathPrefix = $disk->getDriver()->getAdapter()->getPathPrefix();
+                // \Log::info('delete path ->' . $pathPrefix . $projectRef);
+                // Note: need to use File facade here, as Storage doesn't delete
+                File::deleteDirectory($pathPrefix . $projectRef);
             }
+        } catch (Exception $e) {
+            \Log::error('Error deleting media folder ->' . $pathPrefix . $projectRef, [
+                'exception' => $e->getMessage()
+            ]);
+            $this->errors = ['project_delete_media' => ['ec5_223']];
+            return;
         }
     }
 }
