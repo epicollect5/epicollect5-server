85137d471d270aa5a8dcf25676b49ac63a040eb9
mirko77 <mirko.menegazzo@gmail.com>
2023-11-22 11:11:13 +0100

    added tests for "more" pages

---
+++ tests/Http/Controllers/Web/Project/ManageUsersControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Http\Controllers\Web\Project;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+
+class ManageUsersControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+}
