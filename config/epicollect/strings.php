ebc34ce00a8b5a882773e0c1128a48c2a1323cf2
mirko77 <mirko.menegazzo@gmail.com>
2023-11-28 16:06:25 +0100

    Added database tables to strings config file

--- config/epicollect/strings.php
+++ config/epicollect/strings.php
@@ -308,5 +308,18 @@ return [
         'photo' => 'photo',
         'audio' => 'audio',
         'video' => 'video'
+    ],
+    'database_tables' => ['users' => 'users',
+        'projects' => 'projects',
+        'project_structures' => 'project_structures',
+        'project_stats' => 'project_stats',
+        'projects_featured' => 'projects_featured',
+        'project_mapping' => 'project_export_mapping',
+        'project_roles' => 'project_roles',
+        'entries' => 'entries',
+        'branch_entries' => 'branch_entries',
+        'oauth_client_projects' => 'oauth_client_projects',
+        'oauth_clients' => 'oauth_clients',
+        'oauth_access_tokens' => 'oauth_access_tokens'
     ]
 ];
