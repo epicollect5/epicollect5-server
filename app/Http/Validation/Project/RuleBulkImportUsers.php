fd55c32d6dc5e9794d4e4486ac93138619bee534
mirko77 <mirko.menegazzo@gmail.com>
2024-01-25 18:25:28 +0100

    Refactored rule classes

--- app/Http/Validation/Project/RuleBulkImportUsers.php
+++ app/Http/Validation/Project/RuleBulkImportUsers.php
@@ -30,36 +30,36 @@ class RuleBulkImportUsers extends ValidationBase
      * Additional validation checks that a user is allowed to change another user's role
      * We compare the new role and the existing role (if there is one), against the admin user's
      *
-     * @param $adminUser
+     * @param $projectAdmin
      * @param $user
-     * @param $adminUserRole
-     * @param $newUserRole
+     * @param $projectAdminRole
+     * @param $userToAddRole
      * @param $existingUserRole
      */
-    public function additionalChecks($adminUser, $user, $adminUserRole, $newUserRole = null, $existingUserRole = null)
+    public function additionalChecks($projectAdmin, $userToAdd, $projectAdminRole, $userToAddRole = null, $existingUserRole = null)
     {
         // We must have at least one role supplied
-        if (!$newUserRole && !$existingUserRole) {
+        if (!$userToAddRole && !$existingUserRole) {
             $this->errors['user'] = ['ec5_90'];
             return;
         }
 
-        // Check the $adminUser is not the user we're trying to update the role for
-        // i.e. a user cannot change their own role
-        if ($adminUser->id == $user->id) {
+        // Check the $projectAdmin is not the user we're trying to update the role for
+        // Basically a user cannot change their own role
+        if ($projectAdmin->id == $userToAdd->id) {
             $this->errors['user'] = ['ec5_217'];
             return;
         }
 
-        // $adminUser must have a valid role
-        if (!is_array(config('epicollect.permissions.projects.roles.' . $adminUserRole))) {
+        // $projectAdmin must have a valid role
+        if (!is_array(config('epicollect.permissions.projects.roles.' . $projectAdminRole))) {
             $this->errors['user'] = ['ec5_91'];
             return;
         }
 
-        if ($newUserRole) {
-            // $adminUser can only perform actions against certain roles, set in permissions config
-            if (!in_array($newUserRole, config('epicollect.permissions.projects.roles.' . $adminUserRole))) {
+        if ($userToAddRole) {
+            // $projectAdmin can only perform actions against certain roles, set in permissions config
+            if (!in_array($userToAddRole, config('epicollect.permissions.projects.roles.' . $projectAdminRole))) {
                 $this->errors['user'] = ['ec5_91'];
                 return;
             }
@@ -68,7 +68,7 @@ class RuleBulkImportUsers extends ValidationBase
         // If an existing role was passed in, check
         if ($existingUserRole) {
             // $requestedUser can only perform actions against certain roles, set in permissions config
-            if (!in_array($existingUserRole, config('epicollect.permissions.projects.roles.' . $adminUserRole))) {
+            if (!in_array($existingUserRole, config('epicollect.permissions.projects.roles.' . $projectAdminRole))) {
                 $this->errors['user'] = ['ec5_91'];
                 return;
             }
--- app/Http/Validation/Project/RuleProjectRole.php
+++ app/Http/Validation/Project/RuleProjectRole.php
@@ -3,69 +3,56 @@
 namespace ec5\Http\Validation\Project;
 
 use ec5\Http\Validation\ValidationBase;
-use Config;
 
 class RuleProjectRole extends ValidationBase
 {
     protected $rules = [
-        // Allowed roles that can be added for a user (note: 'creator' is not a role that can be assigned after project creation)
         'email' => 'required|email'
     ];
 
-    /**
-     * RuleProjectRole constructor.
-     */
     public function __construct()
     {
-        // Add only valid 'provider' auth methods ie not 'local'
-        $authMethods = config('auth.auth_methods');
+        // Allowed roles that can be added for a user
+        // imp: 'creator' is not a role that can be assigned after project creation
         $roles = config('epicollect.permissions.projects.roles.creator');
-        $invalidAuthMethod = array_search('local', config('auth.auth_methods'));
-        if ($invalidAuthMethod !== false) {
-            unset($authMethods[$invalidAuthMethod]);
-        }
-
-        $this->rules['provider'] = 'in:' . implode($authMethods, ',');
-
-        //role must be know by the system
         $this->rules['role'] = 'required|in:' . implode($roles, ',');
     }
 
     /**
      * Additional validation checks that a user is allowed to change another user's role
-     * We compare the new role and the existing role (if there is one), against the admin user's
+     * We compare the new role and the existing role (if there is one), against the $projectAdmin user's
      *
-     * @param $adminUser
-     * @param $user
-     * @param $adminUserRole
-     * @param $newUserRole
-     * @param $existingUserRole
+     * @param $projectAdmin
+     * @param $userToAdd
+     * @param $projectAdminRole
+     * @param null $userToAddRole
+     * @param null $existingUserRole
      */
-    public function additionalChecks($adminUser, $user, $adminUserRole, $newUserRole = null, $existingUserRole = null)
+    public function additionalChecks($projectAdmin, $userToAdd, $projectAdminRole, $userToAddRole = null, $existingUserRole = null)
     {
         // We must have at least one role supplied
-        if (!$newUserRole && !$existingUserRole) {
+        if (!$userToAddRole && !$existingUserRole) {
             $this->errors['user'] = ['ec5_90'];
             return;
         }
 
-        // Check the $adminUser is not the user we're trying to update the role for
-        // i.e. a user cannot change their own role
-        if ($adminUser->id == $user->id) {
+        // Check the $projectAdmin is not the user we're trying to update the role for
+        // Basically a user cannot change its own role
+        if ($projectAdmin->id === $userToAdd->id) {
             $this->errors['user'] = ['ec5_217'];
             return;
         }
 
-        // $adminUser must have a valid role
-        if (!is_array(config('epicollect.permissions.projects.roles.' . $adminUserRole))) {
+        // $projectAdmin must have a valid role
+        if (!is_array(config('epicollect.permissions.projects.roles.' . $projectAdminRole))) {
             $this->errors['user'] = ['ec5_91'];
             return;
         }
 
-        if ($newUserRole) {
-            // $adminUser can only perform actions against certain roles, set in permissions config
-            if (!in_array($newUserRole, config('epicollect.permissions.projects.roles.' . $adminUserRole))) {
-                $this->errors['user'] = ['ec5_91'];
+        if ($userToAddRole) {
+            // $projectAdmin can only perform actions against lower roles, set in permissions config
+            if (!in_array($userToAddRole, config('epicollect.permissions.projects.roles.' . $projectAdminRole))) {
+                $this->errors['user'] = ['ec5_344'];
                 return;
             }
         }
@@ -73,8 +60,8 @@ class RuleProjectRole extends ValidationBase
         // If an existing role was passed in, check
         if ($existingUserRole) {
             // $requestedUser can only perform actions against certain roles, set in permissions config
-            if (!in_array($existingUserRole, config('epicollect.permissions.projects.roles.' . $adminUserRole))) {
-                $this->errors['user'] = ['ec5_91'];
+            if (!in_array($existingUserRole, config('epicollect.permissions.projects.roles.' . $projectAdminRole))) {
+                $this->errors['user'] = ['ec5_344'];
                 return;
             }
         }
