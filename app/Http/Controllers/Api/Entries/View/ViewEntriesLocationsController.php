9c48b6f513880255fce2a3e8feb1bdaf9063d4d3
mirko77 <mirko.menegazzo@gmail.com>
2024-02-14 12:23:34 +0100

    Refactored and added missing newest and oldest to response

--- app/Http/Controllers/Api/Entries/View/ViewEntriesLocationsController.php
+++ app/Http/Controllers/Api/Entries/View/ViewEntriesLocationsController.php
@@ -2,20 +2,34 @@
 
 namespace ec5\Http\Controllers\Api\Entries\View;
 
-use ec5\Http\Validation\Entries\Search\RuleQueryStringLocations;
+use Carbon\Carbon;
+use ec5\Http\Validation\Entries\View\RuleQueryStringLocations;
 use ec5\Models\Entries\BranchEntry;
 use ec5\Models\Entries\Entry;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Response;
 
 class ViewEntriesLocationsController extends ViewEntriesControllerBase
 {
+    /**
+     * @param RuleQueryStringLocations $ruleQueryStringLocations
+     * @return JsonResponse
+     *
+     * Get the entries' locations (geojson format) belonging to a question
+     *
+     * Hierarchy: form_ref (default to the top parent form), input_ref
+     *
+     * Branch: needs the branch_ref as well
+     *
+     *
+     */
     public function show(RuleQueryStringLocations $ruleQueryStringLocations)
     {
         $allowedKeys = array_keys(config('epicollect.strings.search_data_entries'));
         $perPage = config('epicollect.limits.entries_map.per_page');
         $params = $this->entriesViewService->getSanitizedQueryParams($allowedKeys, $perPage);
 
-        // Validate the params
+        // Validate the payload params values
         $ruleQueryStringLocations->validate($params);
         if ($ruleQueryStringLocations->hasErrors()) {
             return Response::apiErrorCode(400, $ruleQueryStringLocations->errors());
@@ -34,8 +48,19 @@ class ViewEntriesLocationsController extends ViewEntriesControllerBase
             $entryModel = new Entry();
         }
 
-        $entryData = $entryModel->getGeoJsonData($this->requestedProject()->getId(), $params)
-            ->paginate($params['per_page']);
+        $entries = $entryModel->getGeoJsonData($this->requestedProject()->getId(), $params);
+
+        if ($entries->first() !== null) {
+            $oldest = str_replace(' ', 'T', $entries->min('created_at')) . '.000Z';
+            $newest = str_replace(' ', 'T', $entries->max('created_at')) . '.000Z';
+        } else {
+            //no entries, return today's date as ISO string
+            $now = Carbon::now()->toIso8601String();
+            $oldest = str_replace('+00:00', '.000Z', $now);
+            $newest = str_replace('+00:00', '.000Z', $now);
+        }
+
+        $entriesPaginated = $entries->paginate($params['per_page']);
 
         // Data
         $data = [
@@ -46,20 +71,20 @@ class ViewEntriesLocationsController extends ViewEntriesControllerBase
                 'features' => []
             ]
         ];
-        // todo: can this be optimised?
+
         // Loop and json decode the geo json data from the db
-        foreach ($entryData as $row) {
+        foreach ($entriesPaginated as $entry) {
             // Add to the geo json features array if it is NOT NULL
-            if (isset($row->geo_json_data)) {
-                $data['geojson']['features'][] = json_decode($row->geo_json_data, true);
+            if (isset($entry->geo_json_data)) {
+                $data['geojson']['features'][] = json_decode($entry->geo_json_data, true);
             }
         }
 
         // Append the required options to the LengthAwarePaginator
-        $this->appendOptions($entryData, $params);
+        $this->appendOptions($entriesPaginated, $params);
         // Get Meta and Links
-        $meta = $this->getMeta($entryData);
-        $links = $this->getLinks($entryData);
+        $meta = $this->getMeta($entriesPaginated, $newest, $oldest);
+        $links = $this->getLinks($entriesPaginated);
 
         return Response::apiData($data, $meta, $links);
     }
