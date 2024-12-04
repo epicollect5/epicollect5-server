c4b3eb46d25b5593d56564ea3cdd2a69a97662d5
Mirko <mirko.menegazzo@gmail.com>
2024-12-04 19:41:25 +0100

    Fixed migration when users_verify table does not exist

--- database/migrations/2024_11_08_120320_drop_users_verify_table.php
+++ database/migrations/2024_11_08_120320_drop_users_verify_table.php
@@ -8,7 +8,7 @@ return new class () extends Migration {
      */
     public function up(): void
     {
-        Schema::drop('users_verify');
+        Schema::dropIfExists('users_verify');
     }
 
     /**
