61937cdb3d65b5bd0e80cbd0ef145f033b1b38f1
Mirko <mirko.menegazzo@gmail.com>
2025-01-06 18:28:14 +0100

    Added comment about exif only available with imagick

--- config/image.php
+++ config/image.php
@@ -6,8 +6,9 @@ return array(
     |--------------------------------------------------------------------------
     | Image Driver
     |--------------------------------------------------------------------------
-    | imp: Imagick is used to keep the metadata when resizing, as GD was stripping them
+    | imp: Imagick must be used to keep the metadata when resizing, as GD was stripping them
     | imp: EXIF Support	must be enabled, check phpinfo()
+    | see https://github.com/Intervention/image/issues/886
     */
 
     'driver' => 'imagick'
