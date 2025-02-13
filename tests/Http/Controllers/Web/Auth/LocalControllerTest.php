fddc3cd4e1b576ed5535eacb5669d8cbf28dd8d0
Mirko <mirko.menegazzo@gmail.com>
2025-02-13 11:55:00 +0100

    Fixed tests

--- tests/Http/Controllers/Web/Auth/LocalControllerTest.php
+++ tests/Http/Controllers/Web/Auth/LocalControllerTest.php
@@ -5,14 +5,13 @@ namespace Tests\Http\Controllers\Web\Auth;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
-
 class LocalControllerTest extends TestCase
 {
     use DatabaseTransactions;
 
     public function test_login_page_renders_correctly()
     {
-        $response = $this->get(route('login-staff')); // Replace with the actual route or URL to your view
+        $response = $this->get(route('login-admin')); // Replace with the actual route or URL to your view
         $response->assertStatus(200); // Ensure the response is successful
     }
 }
