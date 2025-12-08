3724714645cb855e0e21d0fe6727157b214d8c81
Mirko <mirko.menegazzo@gmail.com>
2025-12-08 11:32:46 +0100

    split s3 and local tests

--- tests/Console/Commands/SystemProjectWiperCommandLocalTest.php
+++ tests/Console/Commands/SystemProjectWiperCommandLocalTest.php
@@ -15,54 +15,48 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
 
-class SystemProjectWiperCommandTest extends TestCase
+class SystemProjectWiperCommandLocalTest extends TestCase
 {
     use DatabaseTransactions;
 
     private int $numOfEntries = 10;
+    private string $originalName = 'Test Project';
+    private Project $project;
 
-    /**
-     * Set up a project structure for testing.
-     * @param string $status
-     * @param string $originalName
-     * @return Project
-     */
-    protected function setupProject(string $status = 'archived', string $originalName = 'Test Project'): Project
+
+    public function setUp(): void
     {
+        parent::setUp();
+        $this->overrideStorageDriver('local');
+
         // 1. Create Project
         $projectRef = Generators::projectRef();
-        $project = factory(Project::class)->create([
-            'status' => $status,
+        $this->project = factory(Project::class)->create([
+            'status' => config('epicollect.strings.project_status.archived'),
             'ref' => $projectRef,
-            'slug' => Str::slug($originalName, '-'),
+            'slug' => Str::slug($this->originalName, '-'),
         ]);
 
-        factory(ProjectStats::class)->create(['project_id' => $project->id]);
+        // 2. Create Project Structure and Stats
+        factory(ProjectStats::class)->create(['project_id' => $this->project->id]);
         factory(ProjectStructure::class)->create(
             [
-                'project_id' => $project->id,
-                'project_definition' => json_encode(['project' => ['name' => $originalName]]),
+                'project_id' => $this->project->id,
+                'project_definition' => json_encode(['project' => ['name' => $this->originalName]]),
                 'updated_at' => now(),
             ]
         );
 
-        // 2. Insert project_structure for original name lookup
-        DB::table('project_structures')->insert([
-            'project_id' => $project->id,
-
-        ]);
-
         // 3. Create Entries and Branch Entries
         for ($i = 0; $i < $this->numOfEntries; $i++) {
-            $entry = Factory(Entry::class)->create(['project_id' => $project->id]);
-            Factory(BranchEntry::class)->create([
-                'project_id' => $project->id,
+            $entry = factory(Entry::class)->create(['project_id' => $this->project->id]);
+            factory(BranchEntry::class)->create([
+                'project_id' => $this->project->id,
                 'owner_entry_id' => $entry->id
             ]);
         }
-
-        return $project;
     }
+
 
     public function test_it_fails_if_project_id_option_is_missing()
     {
@@ -81,35 +75,35 @@ class SystemProjectWiperCommandTest extends TestCase
 
     public function test_it_fails_if_project_is_not_archived()
     {
-        $project = $this->setupProject('active');
+        $this->project->status = 'active';
+        $this->project->save();
 
-        $this->artisan('system:project-wipe', ['--project-id' => $project->id])
+        $this->artisan('system:project-wipe', ['--project-id' => $this->project->id])
             ->expectsOutputToContain("Project must be archived before erasing.")
             ->assertExitCode(defined('Command::FAILURE') ? Command::FAILURE : 1);
     }
 
     public function test_it_fails_on_incorrect_confirmation_name()
     {
-        $project = $this->setupProject('archived');
-        $originalName = 'Test Project';
+        $entriesCountBefore = Entry::count();
+        $branchEntriesCountBefore = BranchEntry::count();
         $wrongName = 'Wrong Name';
 
-        $this->artisan('system:project-wipe', ['--project-id' => $project->id])
-            ->expectsQuestion("To confirm, type the project’s original name: $originalName", $wrongName)
+        $this->artisan('system:project-wipe', ['--project-id' => $this->project->id])
+            ->expectsQuestion("To confirm, type the project’s original name: $this->originalName", $wrongName)
             ->expectsOutputToContain("Confirmation name does not match. Aborting.")
             ->assertExitCode(defined('Command::FAILURE') ? Command::FAILURE : 1);
 
         // Assert data still exists
-        $this->assertDatabaseHas('projects', ['id' => $project->id]);
-        $this->assertDatabaseCount('entries', $this->numOfEntries);
-        $this->assertDatabaseCount('branch_entries', $this->numOfEntries);
+        $this->assertDatabaseHas('projects', ['id' => $this->project->id]);
+        $this->assertDatabaseCount('entries', $entriesCountBefore);
+        $this->assertDatabaseCount('branch_entries', $branchEntriesCountBefore);
     }
 
     public function test_it_runs_in_dry_run_mode_and_lists_deletions()
     {
-        // 1. Setup Project and Entries
-        $project = $this->setupProject('archived');
-        $originalName = 'Test Project';
+        $entriesCountBefore = Entry::count();
+        $branchEntriesCountBefore = BranchEntry::count();
 
         // 2. Setup Storage Fakes
         $mediaDisks = config('epicollect.media.entries_deletable');
@@ -127,32 +121,33 @@ class SystemProjectWiperCommandTest extends TestCase
                     break;
             }
             // Create some files in the project's ref folder
-            Storage::disk($disk)->put($project->ref . '/file1_' . time().$fileExtension, 'content');
-            Storage::disk($disk)->put($project->ref . '/file2_' . time().$fileExtension, 'content');
+            Storage::disk($disk)->put($this->project->ref . '/file1_' . time().$fileExtension, 'content');
+            Storage::disk($disk)->put($this->project->ref . '/file2_' . time().$fileExtension, 'content');
 
         }
 
         // 3. Run Command in Dry-Run
         $command = $this->artisan('system:project-wipe', [
-            '--project-id' => $project->id,
+            '--project-id' => $this->project->id,
             '--dry-run' => true,
         ])
-            ->expectsQuestion("To confirm, type the project’s original name: $originalName", $originalName);
+            ->expectsQuestion("To confirm, type the project’s original name: $this->originalName", $this->originalName);
 
         // 4. Assert Output
         $command->expectsOutputToContain("[DRY-RUN] Listing what would be deleted...");
-        $command->expectsOutputToContain("Disk photo/$project->ref: 2 files would be deleted");
-        $command->expectsOutputToContain("Disk audio/$project->ref: 2 files would be deleted");
-        $command->expectsOutputToContain("Disk video/$project->ref: 2 files would be deleted");
+        $command->expectsOutputToContain("Disk photo/{$this->project->ref}: 2 files would be deleted");
+        $command->expectsOutputToContain("Disk audio/{$this->project->ref}: 2 files would be deleted");
+        $command->expectsOutputToContain("Disk video/{$this->project->ref}: 2 files would be deleted");
         $command->expectsOutputToContain("Entries: $this->numOfEntries rows would be deleted, and related $this->numOfEntries branch entries.");
         $command->expectsOutputToContain("Dry-run complete. No data was deleted.");
         $command->assertExitCode(defined('Command::SUCCESS') ? Command::SUCCESS : 0);
 
         // 5. Assert data still exists
-        $this->assertDatabaseHas('projects', ['id' => $project->id]);
-        $this->assertDatabaseCount('entries', $this->numOfEntries);
+        $this->assertDatabaseHas('projects', ['id' => $this->project->id]);
+        $this->assertDatabaseCount('entries', $entriesCountBefore);
+        $this->assertDatabaseCount('branch_entries', $branchEntriesCountBefore);
         foreach ($mediaDisks as $disk) {
-            $this->assertCount(2, Storage::disk($disk)->allFiles($project->ref));
+            $this->assertCount(2, Storage::disk($disk)->allFiles($this->project->ref));
         }
     }
 
@@ -164,16 +159,12 @@ class SystemProjectWiperCommandTest extends TestCase
         $preWipeProjectStructureCount = DB::table('project_structures')->count();
         $preWipeProjectStatsCount = DB::table('project_stats')->count();
 
-        // 1. Setup Project and Entries
-        $project = $this->setupProject('archived');
-        $originalName = 'Test Project';
 
-        // 2. Setup Storage Fakes
+        // 1. Setup Storage (real, end to end test)
         $mediaDisks = config('epicollect.media.entries_deletable');
         $fileExtension = '.jpg';
 
         foreach ($mediaDisks as $disk) {
-            //  Storage::fake($disk);
             switch ($disk) {
                 case 'photo':
                     $fileExtension = '.jpg';
@@ -184,8 +175,8 @@ class SystemProjectWiperCommandTest extends TestCase
                     break;
             }
             // Create some files in the project's ref folder
-            Storage::disk($disk)->put($project->ref . '/file1_' . time().$fileExtension, 'content');
-            Storage::disk($disk)->put($project->ref . '/file2_' . time().$fileExtension, 'content');
+            Storage::disk($disk)->put($this->project->ref . '/file1_' . time().$fileExtension, 'content');
+            Storage::disk($disk)->put($this->project->ref . '/file2_' . time().$fileExtension, 'content');
         }
 
         // NOTE: You would need to mock the internal methods called by eraseProject
@@ -196,24 +187,24 @@ class SystemProjectWiperCommandTest extends TestCase
         // feature test focuses on the command flow and final state.
 
         // 3. Run Command
-        $this->artisan('system:project-wipe', ['--project-id' => $project->id])
-            ->expectsQuestion("To confirm, type the project’s original name: $originalName", $originalName)
+        $this->artisan('system:project-wipe', ['--project-id' => $this->project->id])
+            ->expectsQuestion("To confirm, type the project’s original name: $this->originalName", $this->originalName)
             ->expectsOutputToContain("Project erased successfully.")
             ->assertExitCode(defined('Command::SUCCESS') ? Command::SUCCESS : 0);
 
         // 4. Assert data is deleted
-        $this->assertDatabaseMissing('projects', ['id' => $project->id]);
-        $this->assertDatabaseCount('entries', $preWipeEntriesCount);
-        $this->assertDatabaseCount('branch_entries', $preWipeBranchEntriesCount);
-        $this->assertDatabaseMissing('project_structures', ['project_id' => $project->id]);
+        $this->assertDatabaseMissing('projects', ['id' => $this->project->id]);
+        $this->assertDatabaseCount('entries', $preWipeEntriesCount - $this->numOfEntries);
+        $this->assertDatabaseCount('branch_entries', $preWipeBranchEntriesCount - $this->numOfEntries);
+        $this->assertDatabaseMissing('project_structures', ['project_id' => $this->project->id]);
 
-        $this->assertDatabaseCount('project_stats', $preWipeProjectStatsCount);
-        $this->assertDatabaseCount('projects', $preWipeProjectCount);
-        $this->assertDatabaseCount('project_structures', $preWipeProjectStructureCount);
+        $this->assertDatabaseCount('project_stats', $preWipeProjectStatsCount - 1);
+        $this->assertDatabaseCount('projects', $preWipeProjectCount - 1);
+        $this->assertDatabaseCount('project_structures', $preWipeProjectStructureCount - 1);
 
-        // 5. Assert media is deleted (Storage::fake deletes the directory when `removeMediaChunk` is called correctly)
+        // 5. Assert media is deleted (whole directory is deleted when `removeMediaChunk` is called correctly)
         foreach ($mediaDisks as $disk) {
-            $this->assertCount(0, Storage::disk($disk)->allFiles($project->ref));
+            $this->assertCount(0, Storage::disk($disk)->allFiles($this->project->ref));
         }
     }
 }
