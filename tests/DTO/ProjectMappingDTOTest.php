6a43d12827a7da0962457e01ef74883d9ec93441
Mirko <mirko.menegazzo@gmail.com>
2026-04-21 12:39:20 +0200

    Update ProjectMappingDTOTest: add tests for default mapping preservation and handling of missing is_default flags

--- tests/DTO/ProjectMappingDTOTest.php
+++ tests/DTO/ProjectMappingDTOTest.php
@@ -3,10 +3,35 @@
 namespace Tests\DTO;
 
 use ec5\DTO\ProjectMappingDTO;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
 class ProjectMappingDTOTest extends TestCase
 {
+    public function test_init_preserves_a_single_valid_default_mapping(): void
+    {
+        $projectMapping = new ProjectMappingDTO();
+
+        $projectMapping->init([
+            0 => [
+                'name' => 'EC5 AUTO',
+                'is_default' => false,
+                'forms' => [],
+                'map_index' => 0
+            ],
+            1 => [
+                'name' => 'Custom',
+                'is_default' => true,
+                'forms' => [],
+                'map_index' => 1
+            ]
+        ]);
+
+        $mappingData = $projectMapping->getData();
+
+        $this->assertFalse($mappingData[0]['is_default']);
+        $this->assertTrue($mappingData[1]['is_default']);
+    }
+
     public function test_init_prefers_custom_default_when_multiple_defaults_exist(): void
     {
         $projectMapping = new ProjectMappingDTO();
@@ -55,6 +80,30 @@ class ProjectMappingDTOTest extends TestCase
 
         $this->assertTrue($mappingData[0]['is_default']);
         $this->assertFalse($mappingData[1]['is_default']);
+    }
+
+    public function test_init_treats_missing_is_default_as_false(): void
+    {
+        $projectMapping = new ProjectMappingDTO();
+
+        $projectMapping->init([
+            0 => [
+                'name' => 'EC5 AUTO',
+                'forms' => [],
+                'map_index' => 0
+            ],
+            1 => [
+                'name' => 'Custom',
+                'is_default' => true,
+                'forms' => [],
+                'map_index' => 1
+            ]
+        ]);
+
+        $mappingData = $projectMapping->getData();
+
+        $this->assertFalse($mappingData[0]['is_default']);
+        $this->assertTrue($mappingData[1]['is_default']);
     }
 
     public function test_init_normalizes_json_loaded_from_the_database(): void
